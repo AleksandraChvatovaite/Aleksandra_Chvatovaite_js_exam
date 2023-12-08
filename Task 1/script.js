00b7fd764a8eb26dc80c5341d84b9f7918d9e095
Aleksandra <aleksandrachvatovaite@gmail.com>
2023-12-08 16:46:41 +0200

    1 task init

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,22 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 div elemente su id "output" viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+
+'use strict';
+console.log('script.js file was loaded');
+
+const els = {
+  formEl: document.forms[0],
+  inputEl: document.getElementById('search'),
+  divOutputEl: document.getElementById('output'),
+}
+
+els.formEl.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const kilograms = els.inputEl.value;
+  const lb = kilograms * 2.2046;
+  const g = kilograms / 0.0010000;
+  const oz = kg * 35.274;
+  els.divOutputEl.append()
+})
+
