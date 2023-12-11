3681094514627186b74b114905b040078f99286b
Aleksandra <aleksandrachvatovaite@gmail.com>
2023-12-11 08:32:53 +0200

    task 8

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,27 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+class Calculator {
+  num1;
+  num2;
+  constructor(sk1, sk2) {
+    this.num1 = sk1;
+    this.num2 = sk2;
+  }
+
+  sum () {
+    return this.num1 + this.num2;
+  }
+
+  subtraction () {
+    return this.num1 - this.num2;
+  }
+
+  multiplication() {
+    return this.num1 * this.num2;
+  }
+  division() {
+    return this.num1 / this.num2;
+  }
+}
+
